6f862aa5ae9ec242217bf129e80ea9441902f593
kennethcasey88 <kennethcasey88@gmail.com>
2024-02-29 22:50:13 +0100

    Created the algorithm to search for a winner

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Create the gameboard and handle the player marks//
 function gameBoard() {
     const board = [];
     const rows = 3;
@@ -18,6 +19,7 @@ function gameBoard() {
         if (!isCellEmpty) return 'cancel';
 
         board[row][index].addMark(mark);
+
     }
 
     const printBoard = () => {
@@ -27,7 +29,7 @@ function gameBoard() {
     
     return { getBoard, placeMark, printBoard };
 };
-
+//This function allows the gameboard to place marks in chosen cells//
 function cell() {
     let value = '';
 
@@ -44,6 +46,7 @@ function createPlayer(name, mark) {
     return { name, mark };
 }
 
+// Here we control the flow of the game using the above functions//
 function gameController() {
     const pappa = createPlayer('Pappa', 'X');
     const leia = createPlayer('Leia', 'O');
@@ -64,26 +67,57 @@ function gameController() {
     };
     
     const playRound = (row, index) => {
+        const board2 = board.getBoard();
         if (board.placeMark(row, index, getActivePlayer().mark) === 'cancel') {
             console.log('invalid move');
             return;
         }
 
+        let roundCount = 0;
+        roundCount++;
+
         console.log(
-          `Putting ${getActivePlayer().name}'s mark into row ${row}, index ${index}...`
+            `Putting ${getActivePlayer().name}'s mark into row ${row}, index ${index}...`
         );
         board.placeMark(row, index, getActivePlayer().mark);
     
+        if (roundCount = 5) {
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    if (board2[i][0].getValue() === board2[i][1].getValue()
+                        && board2[i][0].getValue() === board2[i][2].getValue()
+                        && board2[i][0].getValue() !== '') {
+                        console.log(`${getActivePlayer().name} wins`);
+                        return 'game over';
+                    } else if (board2[0][j].getValue() === board2[1][j].getValue()
+                        && board2[0][j].getValue() === board2[2][j].getValue()
+                        && board2[0][j].getValue() !== '') {
+                        console.log(`${getActivePlayer().name} wins`);
+                        return 'game over';
+                    } else if (board2[0][0].getValue() === board2[1][1].getValue()
+                        && board2[0][0].getValue() === board2[2][2].getValue()
+                        && board2[0][0].getValue() !== '') {
+                        console.log(`${getActivePlayer().name} wins`);
+                        return 'game over';
+                    } else if (board2[0][2].getValue() === board2[1][1].getValue()
+                        && board2[0][2].getValue() === board2[2][0].getValue()
+                        && board2[0][2].getValue() !== '') {
+                        console.log(`${getActivePlayer().name} wins`);
+                        return 'game over';
+                        }
+                }
+            }
+        }
         
     
-        switchPlayer();
-        printNewRound();
-    };
+            switchPlayer();
+            printNewRound();
+        };
     
-    printNewRound();
+        printNewRound();
 
-    return { playRound, getActivePlayer, getBoard: board.getBoard };
-}
+        return { playRound, getActivePlayer, getBoard: board.getBoard };
+    }
 
 const game = gameController();
 
