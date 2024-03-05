5bab3f82017845ebb598da7a38457332901c1e60
kennethcasey88 <kennethcasey88@gmail.com>
2024-03-05 19:37:25 +0100

    Finished the project to check for win/tie and everything works as expected

--- index.js
+++ index.js
@@ -1,87 +1,121 @@
 //Create the gameboard and handle the player marks//
 function gameBoard() {
-    const board = [];
+    let board = [];
     const rows = 3;
     const columns = 3;
-
+  
     for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
             board[i].push(cell());
         }
     };
-
+  
     const getBoard = () => board;
-
+  
     const placeMark = (row, index, mark) => {
         /*const isCellEmpty = board[row][index].getValue() === '';
-
+  
         if (!isCellEmpty) return 'cancel'; 
             */
         board[row][index].addMark(mark);
-
+  
     }
-
+  
+    const resetBoard = () => {
+      for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(cell());
+        }
+    };
+    }
+  
     const printBoard = () => {
         const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
         console.log(boardWithCellValues);
     };
     
-    return { getBoard, placeMark, printBoard };
-};
-//This function allows the gameboard to place marks in chosen cells//
-function cell() {
+    return { getBoard, placeMark, resetBoard, printBoard, };
+  };
+  //This function allows the gameboard to place marks in chosen cells//
+  function cell() {
     let value = '';
-
+  
     const addMark = (player) => {
         value = player;
     };
-
+  
     const getValue = () => value;
-
+  
     return { addMark, getValue };
-};
-
-function createPlayer(name, mark) {
+  };
+  
+  function createPlayer(name, mark) {
     return { name, mark };
-}
-
-// Here we control the flow of the game using the above functions//
-let roundCount = 0;
-function gameController() {
+  }
+  
+  // Here we control the flow of the game using the above functions//
+  let roundCount = 0;
+  function gameController() {
     const pappa = createPlayer('Pappa', 'X');
     const leia = createPlayer('Leia', 'O');
-
+  
     const board = gameBoard();
-
+  
+    let winTieChecker = false;
+    let tieChecker = false;
+    const switchWinTie = () => {
+      winTieChecker = winTieChecker === false ? true : false;
+    }
+    const switchTie = () => {
+      tieChecker = tieChecker === false ? true : false;
+    }
+  
     let activePlayer = pappa;
-
+  
     const switchPlayer = () => {
         activePlayer = activePlayer === pappa ? leia : pappa;
     }
-
+    const getWinTie = () => winTieChecker;
+    const getTieChecker = () => tieChecker;
+  
     const getActivePlayer = () => activePlayer;
-
+  
+    const resetActivePlayer = () => {
+      activePlayer = pappa;
+    }
+  
+    const resetWinTieChecker = () => {
+      winTieChecker = false;
+    }
+  
+    const resetTieChecker = () => {
+      tieChecker = false;
+    }
+  
     const printNewRound = () => {
         board.printBoard();
         console.log(`${getActivePlayer().name}'s turn.`);
     };
     
     const playRound = (row, index) => {
-        const board2 = board.getBoard();
+        
         if (board.placeMark(row, index, getActivePlayer().mark) === 'cancel') {
             console.log('invalid move');
             return;
         }
-
-        
+  
         roundCount++;
-
+  
         console.log(
             `Putting ${getActivePlayer().name}'s mark into row ${row}, index ${index}...`
         );
-        board.placeMark(row, index, getActivePlayer().mark);
-    
+      board.placeMark(row, index, getActivePlayer().mark);
+      
+      
+      printNewRound();
+      let board2 = board.getBoard();
         if (roundCount === 5) {
             for (let i = 0; i < 3; i++) {
                 for (let j = 0; j < 3; j++) {
@@ -89,49 +123,57 @@ function gameController() {
                         && board2[i][0].getValue() === board2[i][2].getValue()
                         && board2[i][0].getValue() !== '') {
                         console.log(`${getActivePlayer().name} wins`);
-                        return 'game over';
+                        return switchWinTie();
                     } else if (board2[0][j].getValue() === board2[1][j].getValue()
                         && board2[0][j].getValue() === board2[2][j].getValue()
                         && board2[0][j].getValue() !== '') {
                         console.log(`${getActivePlayer().name} wins`);
-                        return 'game over';
+                        return switchWinTie();
                     } else if (board2[0][0].getValue() === board2[1][1].getValue()
                         && board2[0][0].getValue() === board2[2][2].getValue()
                         && board2[0][0].getValue() !== '') {
                         console.log(`${getActivePlayer().name} wins`);
-                        return 'game over';
+                        return switchWinTie();
                     } else if (board2[0][2].getValue() === board2[1][1].getValue()
                         && board2[0][2].getValue() === board2[2][0].getValue()
                         && board2[0][2].getValue() !== '') {
                         console.log(`${getActivePlayer().name} wins`);
-                        return 'game over';
-                        }
+                        return switchWinTie();
+                    } 
                 }
             }
+        } else if (roundCount === 9 && (!board2.some(row => row.includes('')))) {
+          return switchTie();
         }
         
-    
-            switchPlayer();
-            printNewRound();
+        switchPlayer();
+            
         };
     
         printNewRound();
-
-        return { playRound, getActivePlayer, getBoard: board.getBoard };
+  
+    return {
+      playRound, resetActivePlayer, getTieChecker,
+      getActivePlayer, getWinTie, getBoard: board.getBoard,
+      resetBoard: board.resetBoard,
+      resetTieChecker,
+      resetWinTieChecker
+    };
     }
-
-
-function screenController() {
+  
+  
+  function screenController() {
     const game = gameController();
     const display = document.querySelector('.display');
     const gamePad = document.querySelector('.game-pad');
-
+    const restart = document.querySelector('#restart');
+  
     const updateScreen = () => {
         gamePad.textContent = '';
-
+  
         const board = game.getBoard();
         const activePlayer = game.getActivePlayer();
-
+  
         display.textContent = `${activePlayer.name}'s turn...`;
         let data = 0;
         board.forEach(row => {
@@ -144,11 +186,29 @@ function screenController() {
             })
           })
     }
+    function clearBoard() {
+      game.resetWinTieChecker();
+      roundCount = 0;
+      game.resetActivePlayer();
+      game.resetTieChecker();
+      display.textContent = `${game.getActivePlayer().name}'s turn.`;
+      game.resetBoard();
+      gamePad.addEventListener("click", clickHandlerBoard);
+      // Update the screen after resetting the board
+      updateScreen();
+    }
+  
+    function gameOver() {
+      gamePad.removeEventListener('click', clickHandlerBoard);
+    }
+  
     function clickHandlerBoard(e) {
+      
         const selectedCell = e.target.id;
         /*let round = game.playRound();*/
         if (e.target.textContent === 'X' || e.target.textContent === 'O') return;
         if (!selectedCell) return;
+        
         let row;
         let newId;
     
@@ -164,21 +224,21 @@ function screenController() {
           row = 2;
           game.playRound(row, newId);
         }
-    
-        /*if (round === 'Player1') {
-          display.textContent = 'Player 1 wins!';
-          return;
-        } else if (round === 'Player2') {
-          display.textContent = 'Player 2 wins!';
-          return;
-        }*/
-    
         updateScreen();
+      if (game.getWinTie() === true) {
+        gameOver();
+          return display.textContent = `${game.getActivePlayer().name} wins. RESTART GAME!`;
+      } else if (game.getTieChecker() === true) {
+        gameOver();
+          return display.textContent = 'Tie game. RESTART GAME!';
+          }
+    
       }
-      gamePad.addEventListener("click", clickHandlerBoard);
+    gamePad.addEventListener("click", clickHandlerBoard);
+    restart.addEventListener("click", clearBoard);
     
       // Initial render
       updateScreen();
-}
-
-screenController();
+  }
+  
+  screenController();
